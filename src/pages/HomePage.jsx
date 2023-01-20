7d7e78b1e7083a070022e43c21860c7cf273896e
Mahdi <meiti.edu@gmail.com>
2023-01-20 12:59:16 +0330

    create landingpage hero

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../components/Header";
+import LandingPage from "./LandingPage";
 
 const HomePage = () => {
   return (
     <div className="HomePage">
       <Header />
+      <LandingPage />
     </div>
   );
 };
---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const LandingPage = () => {
+  return (
+    <div className="landing-page">
+      <a href="#" className="landing-page__hero">
+        <img src="./assets/main/intro.jpg" alt="" />
+      </a>
+    </div>
+  );
+};
+
+export default LandingPage;
